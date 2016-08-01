e12bba7c199c5356cadc7de608f38b34c1fe47c1
SWPUliusong <1418441382@qq.com>
2016-08-01 11:04:08 +0800

    小改动

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express")
 var filter = require("./filter")
+var logger = require('morgan');
 var app = express()
 var port = process.env.port||3000
 var dbUrl = "mongodb://localhost:27017/bookstore"
@@ -15,6 +16,7 @@ var frouter = require("frouter")   //文件路径即路由
 app.locals = filter    //为ejs设置全局属性
 app.set("views", __dirname + "/views")   //设置模板根路径
 app.set("view engine", "ejs")   //设置模板引擎
+app.use(logger("dev"))
 app.use(bodyparser.json())  //调用body-parser解析json数据
 app.use(bodyparser.urlencoded({extended:false}))    //调用body-parser解析表单数据
 app.use(express.static(__dirname+"/static"))
